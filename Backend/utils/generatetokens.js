4de0948613cf2b18e8fa4d00d648b4fab9b8fe70
abhishekrgadad <abhishekgadad485@gmail.com>
2024-11-21 23:46:11 +0530

    Added secure in generatedtokens

--- Backend/utils/generatetokens.js
+++ Backend/utils/generatetokens.js
@@ -9,9 +9,7 @@ import jwt from 'jsonwebtoken';
         maxAge: 15 * 24 * 60 * 60 * 1000, //MS
         httponly: true, //Prevents from cross-site scripting attacks
         sameSite: "strict",//Prevents from CSRF request forgery attacks
+        secure: process.env.NODE_ENV !== "development",//inorder to generate cookie only in HTTPS secure connections to prevent from attacks
     });
  };
-
-
-
  export default generateTokenAndSetCookie;
