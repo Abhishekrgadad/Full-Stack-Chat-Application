dec5de162e35b93eb083be8356cd559d462d1296
abhishekrgadad <abhishekgadad485@gmail.com>
2024-11-21 18:40:41 +0530

    added API auth Routes and Controllers for signup, login and logout

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,22 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
 
+
+
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req,res)=>{
     //root route http://localhost:5000/
     res.send("hello world");
 });
 
-app.listen(5000,()=> console.log("Server is running on port 5000"));
+//we are using middleware because its time waste to create new routes for each one
+app.use("/api/auth", authRoutes);
+
+
+
+app.listen(PORT,()=> console.log(`Server is Running on port ${PORT}`));    
